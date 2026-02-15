45517d4f4b2c40a0882eb2572a94c133bf98f459
Hans <hans.mascardo@gmail.com>
2026-02-15 21:54:55 +0800

    Added top review preview in inset and review filters

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // npm install express
 
 const express = require("express");
-const exphbs = require('express-handlebars')
+const exphbs = require("express-handlebars");
 const fs = require("fs");
 const path = require("path");
 
@@ -34,8 +34,24 @@ app.set("views", path.join(__dirname, "views"));
 // Serve everything inside /public
 app.use(express.static(path.join(__dirname, "public")));
 
+// Load restaurant list
 const restaurantListPath = path.join(__dirname, "public", "processes", "restaurant_list.json");
 const restaurantList = JSON.parse(fs.readFileSync(restaurantListPath, "utf8"));
+
+// Load users database once
+const users = require(path.join(__dirname, "public", "data", "users", "users.json")).users;
+
+// Helper to resolve profile picture safely
+function resolveProfilePic(profilePicPath) {
+  const normalizedPath = path.join(__dirname, "public", profilePicPath.replace(/^(\.\.\/)+/, ""));
+  // Check if file exists
+  if (fs.existsSync(normalizedPath)) {
+    return profilePicPath.replace(/^(\.\.\/)+/, "/");
+  } else {
+    return "/img/default_profile.png";
+  }
+}
+
 
 // Dynamic route for reviews
 app.get("/pages/restaurants/:id", (req, res) => {
@@ -53,14 +69,19 @@ app.get("/pages/restaurants/:id", (req, res) => {
     reviewsData = JSON.parse(fs.readFileSync(reviewsFile, "utf8"));
   }
 
-
-  //const reviewsData = JSON.parse(fs.readFileSync(reviewsFile, "utf8"));
+  const enrichedReviews = reviewsData.map(r => {
+    const user = users.find(u => u.username === r.user);
+    return {
+      ...r,
+      profilePic: user ? resolveProfilePic(user.profilePic) : "/img/profiles/default.png"
+    };
+  });
 
   res.render("review-template", {
     layout: "main",
     restaurant,
-    reviews: reviewsData,
-    hasReviews: reviewsData.length > 0
+    reviews: enrichedReviews,
+    hasReviews: enrichedReviews.length > 0
   });
 });
 
